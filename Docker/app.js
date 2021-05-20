89b774424c3108ed4b8d8b76d70e61b418ac74fa
Yasuhiko Nara <narayasuhiko3@gmail.com>
2021-05-20 10:15:56 +0900

    cleaned code

--- Docker/app.js
+++ Docker/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 var app = function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Good Morning!\n');
+  res.end('HELLO\n');
 };
 
 http.createServer(app).listen(3000);
